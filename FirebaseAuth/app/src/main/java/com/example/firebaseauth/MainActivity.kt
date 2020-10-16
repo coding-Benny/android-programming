852e0ea2d6518db0e1ca9c91e143fcb1bd2899f3
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-10-16 11:35:10 +0900

    🔐 sign-in & sign up with firebase

---
+++ FirebaseAuth/app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -0,0 +1,36 @@
+
+package com.example.firebaseauth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebaseauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.textView.text = auth.currentUser?.uid ?: "No User"
+
+        binding.buttonSignout.setOnClickListener {
+            auth.signOut()
+            startActivity(
+                Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+}
